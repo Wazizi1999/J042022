cb2289db3596a2ea9ce2e9c15fa3d6e631ec8945
Wazizi1999 <aziziwahid37@gmail.com>
2022-06-13 11:33:03 -0500

    first commit
    first commit and push to github

---
+++ BigONotation/src/bigo/BigOMain.java
@@ -0,0 +1,41 @@
+package bigo;
+
+public class BigOMain {
+
+	private static String exmapleTwo;
+
+	public static void main(String[] args) {
+		String exampleTwo;
+		// 0(1)Notation: the program will execute in the same time, regardless input size 
+		// increases
+		System.out.println("H");
+		String example = "i am 22 years old"
+		for (int i = 0; i < exampleTwo.length(); i++) {
+		String exmapleTwo;
+		System.out.println("OUTRE LOOP " + i + "CHRACTER " + exmapleTwo.charAt(i) );
+		for (int j = i; j < exampleTwo.length(); j++) {
+		System.out.println("INNER LOOP " + j + "CHARCTER " + exampleTwo.charAt(j));
+		
+		
+		}
+		
+		
+			
+		}
+	
+		//0(log N) Notation running time grows in proportion to the input size 
+		int x = exmapleTwo.length();
+		while (x > 0) {
+			System.out.println(exmapleTwo.charAt(x));
+			
+		}
+				
+		System.out.println(example);
+		
+	
+		
+				
+
+	}
+
+}
